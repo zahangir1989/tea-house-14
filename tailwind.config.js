ea4144089530ed3c652d1323f767e9ce65c3ea5f
zahangir <alamzahangir218@gmail.com>
2025-03-27 00:24:30 +0600

    header section is added

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// /** @type {import('tailwindcss').Config} */
+// MediaSourceHandle.exports = {
+//     content: ['./src/**/*.{html,js}'],
+//     them: {
+//         extend: {},
+//     },
+//     plugins: [],
+// }
